007920e04ba02bf1b116120614308498905659af
Horsia Alin <alin_iulia_2007@yahoo.com>
2022-10-01 13:33:39 +0300

    PCMS_4:Implement a way for the user to interect with the system.

--- src/main/java/com/sda/alinhorsia/petclinic/Main.java
+++ src/main/java/com/sda/alinhorsia/petclinic/Main.java
@@ -4,10 +4,37 @@ import com.sda.alinhorsia.petclinic.controller.VetController;
 import com.sda.alinhorsia.petclinic.repository.VetRepositoryImpl;
 import com.sda.alinhorsia.petclinic.service.VetServiceImpl;
 import com.sda.alinhorsia.petclinic.utils.SessionManager;
+import com.sda.alinhorsia.petclinic.utils.UserOption;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        SessionManager.getSessionFactory();
         VetController vetController = new VetController(new VetServiceImpl(new VetRepositoryImpl()));
-        vetController.createVet();
+
+        UserOption userOption;
+        Scanner scanner = new Scanner(System.in);
+        do{
+            UserOption.displayAllOptions();
+            System.out.println("Please select an option: ");
+            int numericOption = scanner.nextInt();
+            userOption = UserOption.findByNumericOption(numericOption);
+            switch (userOption){
+                case ADD_VET:
+                    vetController.createVet();
+                    break;
+                case VIEW_ALL_VETS:
+                    System.out.println("Not implemented!");
+                    break;
+                case UNKNOWN:
+                    System.err.println("!INVALID OPTION SELECTED!");
+                    break;
+                case EXIT:
+                    System.out.println("Bye!");
+                    break;
+            }
+        }while (userOption != UserOption.EXIT);
+        SessionManager.shutDown();
     }
 }
